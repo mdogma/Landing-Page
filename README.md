dbf542e5715864156a53eb1650c15848fd865174
mdogma <ru.mickael@gmail.com>
2021-08-29 19:59:00 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Landing Page Project for Udacity
+
+##  Project description
+
+The project is a presentation page for a professional naturopath aiming to reach users who could potentially become their patients. 
+
+The page explains what lies behind naturopathy, a quick description of the practitioner and their services, and their contact details.
+
+## Demo
+
+[Click here for the demo](https://duckduckgo.com)
+
+## Development
+
+- The content is divided in 4 sections and the page includes a menu helping users to navigate through these sections. Sections are highlighted when appearing in the viewport for better readability.   
+
+- The menu is created then added to the loaded page with pure JavaScript code. 
+
+- Each item from the menu can be clicked and directs the user to the appropriate section.
+
+## How to download this project?
+
+1. Press “<>Code” tab.
+2. Click on the “Code” button on the right. A dropdown should appear.
+3. In the “Clone” menu, under the “HTTPS” tab, click on “Download ZIP”
+
+## Author
+
+Mickael Rudnicki
---
+++ js/app.js
@@ -0,0 +1,164 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Define Global Variables
+ * 
+ * 
+ * 
+*/
+
+const navList = document.getElementById("navbar__list");
+
+const aLinks = document.getElementsByTagName('a');
+
+const sections = document.querySelectorAll('section');
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// build the nav
+
+const navCreation = (elements) => {
+     for (element of elements) {
+        const newLi = document.createElement('li');
+        const newItem = document.createElement('a');
+        newItem.setAttribute('data-link', `${element.id}`);
+        newItem.classList.add("scroll_to");
+        newItem.textContent = element.getAttribute('data-nav');
+        navList.appendChild(newLi);
+        newLi.appendChild(newItem);
+    };
+}
+
+// Add class 'active' to section when section reaches the top of the viewport
+
+function turnSectionActive(elements) {
+    for (element of elements) {
+      const rect = element.getBoundingClientRect();
+
+      if (rect.top <= 190 && rect.bottom >= 190) {
+        element.classList.add('active');
+      } else {
+        element.classList.remove('active');
+      }
+    }
+ }
+
+
+// Scroll to anchor ID using scrollIntoView event
+
+
+function linker() {
+    links.forEach((link) => {
+    link.addEventListener('click',() => {
+        const dataRelated = document.getElementById(link.getAttribute('data-link'));
+        dataRelated.scrollIntoView({behavior:'smooth', block: 'center'})
+    })
+  })
+}
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+
+navCreation(sections);
+
+// Scroll to section on link click
+
+const links = document.querySelectorAll('.scroll_to');
+
+linker();
+
+
+// Set sections as active
+
+document.addEventListener("scroll",() => {
+    turnSectionActive(sections);
+  });
+
+
+// Toggle Menu
+
+// Creating hidden toggle menu
+
+// 
+const navMenu = document.querySelector('.navbar__menu');
+const respMenu = document.createElement('div');
+respMenu.classList.add('burger');
+
+navMenu.appendChild(respMenu);
+
+// Building our burger lines with 3 empty divs on top of each others that we will color with CSS
+
+for (let i = 1 ; i <= 3 ; i++) {
+  const newDivs = document.createElement('div');
+  respMenu.appendChild(newDivs);
+  newDivs.setAttribute('class', 'line'+[i])
+}
+
+
+// Function allowing the hidden menu to show up progressively in the viewport once our burger icon is clicked on
+
+const navSlide = () => { 
+  const burger = document.querySelector('.burger');
+  const navLinks = document.querySelectorAll('#navbar__list li');
+
+  burger.addEventListener('click',()=> {
+      // Toggle active class for the navigation bar and our hidden menu
+    navList.classList.toggle('nav-active');
+      // Animation effect to slowly bring the <li> up to the screen
+    navLinks.forEach((navli, index) => { 
+      navli.style.animation = `navLinkFade 0.5s ease forwards ${index/ 7 + 0.7}s`;
+  })
+})
+}
+
+// Calling our function navSlide
+
+navSlide();
+
+
+// Hide navigation bar when scrolling down / Show when scrolling back up 
+
+let lastScrollTop = 0;
+
+window.addEventListener('scroll',() => {
+
+  const navWrapper = document.querySelector('.page__header');
+  const scrollTop = window.pageYoffset || document.documentElement.scrollTop;
+  
+  if (scrollTop > lastScrollTop){
+    // Hide navigation bar 
+    navWrapper.style.top="-80px";
+  } else {
+    // Show navigation bar 
+    navWrapper.style.top="0";
+  }
+  lastScrollTop = scrollTop;
+});
